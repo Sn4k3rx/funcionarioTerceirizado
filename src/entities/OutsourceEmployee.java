ee590d0da58f97166edef856588888d3a22778d7
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-22 15:58:04 -0300

    Implementação da classe do funcionario terceirizado, que é herdeira da classe funcionario

--- src/entities/OutsourceEmployee.java
+++ src/entities/OutsourceEmployee.java
@@ -1,5 +1,27 @@
 package entities;
 
-public class OutsourceEmployee {
+public class OutsourceEmployee extends Employee {
+	
+	private Double additionalCharge;
+	
+	public OutsourceEmployee() {
+		super();
+	}
 
+	public OutsourceEmployee(String name, Integer hours, Double valuePerHours, Double additionalCharge) {
+		super(name, hours, valuePerHours);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	public Double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
 }
