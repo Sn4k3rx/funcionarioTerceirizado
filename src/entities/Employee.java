bdb7987e248bc6e259235e5610bb20427aafb516
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-22 15:45:14 -0300

    Implementação da classe do funcionario

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,5 +1,46 @@
 package entities;
 
 public class Employee {
+	
+	private String name;
+	private Integer hours;
+	private Double valuePerHours;
+	
+	public Employee() {
+		
+	}
 
+	public Employee(String name, Integer hours, Double valuePerHours) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHours = valuePerHours;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHours;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHours = valuePerHour;
+	}
+	
+	public Double payment() {
+		return hours * valuePerHours;
+	}
 }
